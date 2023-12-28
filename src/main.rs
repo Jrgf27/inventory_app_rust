74f3d08b0bedaf34d5e8b1906ba492f40fe618a4
Joao Fernandes <ricardo.jfernandes27@gmail.com>
2023-12-28 20:55:02 +0000

    added initial project roots

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use axum::{
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+use tera::{Context, Tera};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> Html<String> {
+    let tera = Tera::new("templates/*").unwrap();
+
+    let context = Context::new();
+
+    // let body = include_str!("../templates/index.html");
+    let output = tera.render("index.html", &context);
+
+    Html(output.unwrap())
+}
